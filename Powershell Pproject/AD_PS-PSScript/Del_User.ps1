6719ae3fc2cd79344f3b56169878ff3e167353ff
Taralp33 <143668097+Taralp33@users.noreply.github.com>
2024-11-20 07:42:32 -0500

    Add files via upload

---
+++ Powershell Pproject/AD_PS-PSScript/Del_User.ps1
@@ -0,0 +1,13 @@
+﻿# Path to the Notepad file containing the usernames #
+$userList = Get-Content -Path ".\names.txt"
+
+# Loop through each username and disable the account #
+foreach ($n in $userList) {
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
+    Disable-ADAccount -Identity $username
+    Write-Host "Deactivated user: $username"
+}
+
+Write-Host "All specified users have been deactivated."
